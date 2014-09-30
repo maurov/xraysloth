a50984497392763563ab471213bb020764ec5529
Mauro Rovezzi <mauro.rovezzi@gmail.com>
2014-09-30 17:05:53 +0200

    rowland tests

---
+++ examples/rowland_tests.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Tests/Examples for rowland
+"""
+
+__author__ = "Mauro Rovezzi"
+__email__ = "mauro.rovezzi@gmail.com"
+__license__ = "BSD license <http://opensource.org/licenses/BSD-3-Clause>"
+__organization__ = "European Synchrotron Radiation Facility"
+__year__ = "2014"
+__version__ = "0.0.3"
+__status__ = "in progress"
+__date__ = "Sept 2014"
+
+from rowland import cs_h, acenx, RcHoriz, RcVert
+
+### TESTS ###
+def testSagOff(Rm, theta0, aXoff, aL=100.):
+    rc = RcHoriz(Rm, theta0, aL=aL, showInfos=True)
+    rc.getSagOff(aXoff)
+
+def testChiOpt():
+    #from specfiledatawriter import SpecfileDataWrite
+    #fname = 'testChiOpt.spec'
+    #sfout = SpecfileDataWriter(fname)
+    ths = np.linspace(34., 86., 27.) #theta scan
+    rms = [250., 500.]
+    als = [0., 25., 50., 100.]
+    agxs = [0., 5.]
+    dres = {'rm' : [],
+            'th' : [],
+            'al' : [],
+            'chi' : [],
+            'axoff' : [],
+            'sagoff' : [],
+            'chi0' : [],
+            'axoff0' : [],
+            'sagoff0' : []}
+    for rm in rms:
+        for al in als:
+            rc = RcHoriz(rm, aL=al, showInfos=False)
+            axoff = acenx(5, asx=25., agx=5.)
+            for th in ths:
+                rc.setTheta0(th)
+                #[math.degrees(rchi), aXoff, SagOff, math.degrees(rchi0), aXoff0, SagOff0]
+                lso = rc.getSagOff(axoff, retAll=True)
+                dres['rm'].append(rm)
+                dres['th'].append(th)
+                dres['al'].append(al)
+                dres['chi'].append(lso[0])
+                dres['axoff'].append(lso[1])
+                dres['sagoff'].append(lso[2])
+                dres['chi0'].append(lso[3])
+                dres['axoff0'].append(lso[4])
+                dres['sagoff0'].append(lso[5])
+    return dres
+
+
+if __name__ == "__main__":
+    #pass
+    testSagOff(250., 35., 150., aL=12.)
+    #dres = testChiOpt()
