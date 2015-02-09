b060e1958bb107eba1b3d7cbc9e4b8fd4dd35b7e
Mauro Rovezzi <mauro.rovezzi@gmail.com>
2015-02-09 21:33:44 +0100

    rm local versioning

--- sloth/rowland.py
+++ sloth/rowland.py
@@ -44,10 +44,7 @@ __email__ = "mauro.rovezzi@gmail.com"
 __credits__ = ""
 __license__ = "BSD license <http://opensource.org/licenses/BSD-3-Clause>"
 __organization__ = "European Synchrotron Radiation Facility"
-__year__ = "2014"
-__version__ = "0.0.4"
-__status__ = "in progress"
-__date__ = "Oct 2014"
+__year__ = "2014--2015"
 
 import sys, os, math
 import numpy as np
