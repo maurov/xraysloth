f48592a27f2180d14c1db99da226542566ab7faa
Mau Rov <mauro.rovezzi@gmail.com>
2015-11-20 17:40:49 +0100

    improved imageview

--- sloth/specfile_with_edf_stack.py
+++ sloth/specfile_with_edf_stack.py
@@ -29,13 +29,15 @@ Related
 
 __author__ = "Mauro Rovezzi"
 __email__ = "mauro.rovezzi@gmail.com"
-__credits__ = ""
+__credits__ = "Thomas Vincent"
 __license__ = "BSD license <http://opensource.org/licenses/BSD-3-Clause>"
 __organization__ = "European Synchrotron Radiation Facility"
 __year__ = "2015"
 
 import os, sys
+import copy
 import numpy as np
+import weakref
 
 ### Matplotlib imports
 import matplotlib.pyplot as plt
@@ -44,16 +46,68 @@ from matplotlib import gridspec
 
 ### PyMca5 imports
 from PyMca5.PyMcaIO import EdfFile
+from PyMca5.PyMcaGui import PyMcaQt as qt
 from PyMca5.PyMcaGui.plotting import MaskImageWidget, ImageView
 
 ### local imports
 from specfiledata import SpecfileData
 
+### UTIL CLASS ###
+class RadarViewWithOverlay(ImageView.RadarView):
+    """RadarView subclass adding a text overlay and a limitsChanged
+listener."""
+
+    def __init__(self, imageView, *args, **kwargs):
+        self._imageView = weakref.proxy(imageView)
+        self._text = ''
+        self._imageView._imagePlot.sigPlotSignal.connect(self.limitsChanged)
+
+        super(RadarViewWithOverlay, self).__init__(*args, **kwargs)
+
+    def limitsChanged(self, event):
+        if event['event'] != 'limitsChanged':
+            return
+
+        # Get histogram info for each axis
+        histoX = self._imageView.getHistogram('x')
+        xHisto = histoX['data']  # histogram
+        xStart = histoX['extent'][0]  # Start column
+        xEnd = histoX['extent'][1]  # End column
+
+        histoY = self._imageView.getHistogram('y')
+        yHisto = histoY['data']  # histogram
+        yStart = histoY['extent'][0]  # Start row
+        yEnd = histoY['extent'][1]  # End row
+
+        # InfoBox
+        self._text = """X: [%d, %d[
+\tmin: %g
+\tmax: %g
+
+Y: [%d, %d[
+\tmin: %g
+\tmax: %g""" % (xStart, xEnd, xHisto.min(), xHisto.max(),
+                yStart, yEnd, yHisto.min(), yHisto.max())
+
+        self.update()
+
+    def drawForeground(self, painter, rect):
+        painter.save()
+        painter.setWorldMatrixEnabled(False)
+        painter.setPen(qt.QPen(qt.Qt.black, 2))
+        painter.drawText(painter.viewport(),
+                         qt.Qt.AlignHCenter | qt.Qt.AlignTop,
+                         self._text)
+        painter.setWorldMatrixEnabled(True)
+        painter.restore()
+
+### MAIN CLASS ###
 class SpecWithEdfStack(SpecfileData):
     """scan data as a SPEC file plus a stack of EDF images"""
 
     def __init__(self, spec_fname, spec_scanno, anim_title=None,\
-                 edf_root=None, edf_dir=None, edf_ext=None, **kws):
+                 edf_root=None, edf_dir=None, edf_ext=None,
+                 noisy_pxs=None, **kws):
         """load SPEC and EDF data
 
         Parameters
@@ -74,6 +128,9 @@ class SpecWithEdfStack(SpecfileData):
         edf_ext : string, images extension
                   if None, '.edf' is taken as default
 
+        noisy_pxs : list of tuples, (X,Y) coordinates of noisy pixels
+                    to set to 0
+
         **kws : as in SpecfileData
 
         """
@@ -82,7 +139,9 @@ class SpecWithEdfStack(SpecfileData):
         # init image plot window
         #self.miw = MaskImageWidget.MaskImageWidget()
         self.miw = ImageView.ImageViewMainWindow()
-
+        _radarview = RadarViewWithOverlay(self.miw.imageView)
+        self.miw.imageView.setRadarView(_radarview)
+        
         # animation figure layout
         self.anim_fig = plt.figure(num='SpecWithEdfStack', figsize=(5,5), dpi=150)
         gs = gridspec.GridSpec(3, 2)
@@ -101,7 +160,9 @@ class SpecWithEdfStack(SpecfileData):
         # load scan
         self.sd = self.sf.select(str(spec_scanno))
         self.mots = dict(zip(self.sf.allmotors(), self.sd.allmotorpos()))
-        self.x = self.sd.datacol(self.sd.alllabels()[0])
+        cntx = kws.get('cntx', self.sd.alllabels()[0])
+        self.x = self.sd.datacol(cntx)
+        
         if self.cmon is not None:
             self.mon = self.sd.datacol(self.cmon)
         if self.csec is not None:
@@ -109,22 +170,42 @@ class SpecWithEdfStack(SpecfileData):
 
         # load images stack
         if edf_root is None:
-            edf_root = '{0}_{1}_'.format(self.fname.split(os.sep)[-1],
-                                         spec_scanno)
+            self.edf_root = '{0}_{1}_'.format(self.fname.split(os.sep)[-1],
+                                              spec_scanno)
+        else:
+            self.edf_root = edf_root
         if edf_dir is None:
-            edf_dir = os.path.join(os.path.dirname(self.fname), 'edf')
+            self.edf_dir = os.path.join(os.path.dirname(self.fname), 'edf')
+        else:
+            self.edf_dir = edf_dir
         if edf_ext is None:
-            edf_ext = '.edf'
+            self.edf_ext = '.edf'
+        else:
+            self.edf_ext = edf_ext
+        self.load_imgs(noisy_pxs=noisy_pxs)
+
+    def load_imgs(self, **kws):
+        """load images in self.imgs list"""
+        noisy_pxs = kws.get('noisy_pxs', None)
         self.imgs = []
         self.imgs_head = []
         self.imgs_int = []
         for idx, x in enumerate(self.x):
-            _fname = '{0}{1}{2}{3:04d}{4}'.format(edf_dir, os.sep,
-                                                  edf_root, idx, edf_ext)
+            _fname = '{0}{1}{2}{3:04d}{4}'.format(self.edf_dir,
+                                                  os.sep,
+                                                  self.edf_root, idx,
+                                                  self.edf_ext)
             #print(_fname)
             edf = EdfFile.EdfFile(_fname, "rb")
             data = edf.GetData(0)
             header = edf.GetHeader(0)
+            if (noisy_pxs is not None) and type(noisy_pxs == list):
+                for px in noisy_pxs:
+                    try:
+                        data[px[1], px[0]] = 0
+                    except:
+                        print("WARNING: something wrong with 'noisy_pxs'")
+                        continue
             _int = np.trapz(np.trapz(data))
             self.imgs.append(data)
             self.imgs_head.append(header)
