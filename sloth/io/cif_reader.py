128a823bf000013411fce4e1b793f25806109cdf
Mauro Rovezzi <mauro.rovezzi@gmail.com>
2018-06-16 21:37:33 +0200

    evaluate more cif readers, cif2feff from pymatgen, still WIP

--- sloth/io/cif_reader.py
+++ sloth/io/cif_reader.py
@@ -5,6 +5,12 @@
 
 """
 
+import os, sys
+import re
+import logging
+
+_logger = logging.getLogger(__name__)
+
 HAS_CifFile = False
 try:
     import CifFile
@@ -66,7 +72,7 @@ class CIFReader(object):
         """
 
         if not HAS_CifFile:
-            print('PyCifRW not found!')
+            _logger.error('CifFile not found! (is PyCifRW installed?)')
             return
 
         self.cif_fname = cif_fname
@@ -147,13 +153,152 @@ class CIFReader(object):
             atline = []
 
         return atoms
-            
+
+def readCifFile(cifFile):
+    """read CIF file with PyCifRW
+
+    Forked from: https://github.com/danmichaelo/cif2vasp/blob/master/cif2vasp.py
+
+    ##################################
+    .. warning:: BUGGY, NOT WORKING!!!
+    ##################################
+    
+    """
+    if not HAS_CifFile:
+        _logger.error('CifFile not found (is PyCifRW installed?)')
+        return
+
+    if not os.path.exists(cifFile):
+        raise IOError("CIF file '%s' was not found!" % (cifFile))
+    
+    cf = CifFile.CifFile(cifFile)
+    print("------------------------------------------------------------------")
+    if len(cf) != 1:
+        raise StandardError("The cif file contains %i data blocks, while one was expected")
+        # A cif file can contain several "datablocks" that each start
+        # with "data_".
+    
+    cb = cf[cf.keys()[0]]                               # open the first block
+    AA = float(re.match('([0-9.]*)',cb['_cell_length_a']).group(0))
+    BB = float(re.match('([0-9.]*)',cb['_cell_length_b']).group(0))
+    CC = float(re.match('([0-9.]*)',cb['_cell_length_c']).group(0))
+    alpha = float(cb['_cell_angle_alpha'])
+    beta = float(cb['_cell_angle_beta'])
+    gamma = float(cb['_cell_angle_gamma'])
+    SG = int(cb['_symmetry_Int_Tables_number'])              # spacegroup
+  
+    atomTypes = []
+    atoms = ''
+    fracOccFound = False
+    firstAtom = True
+    atoms = []
+    for atom in cb.GetLoop('_atom_site_label'):
+        atomKeys = dir(atom)
+        if '_atom_site_type_symbol' in atomKeys:
+            m = re.match('[a-z]*',atom._atom_site_type_symbol,re.I)
+            atomType = m.group(0)
+        else:
+            m = re.match('[a-z]*',atom._atom_site_label,re.I)
+            atomType = m.group(0)
+        
+        atomLabel = atom._atom_site_label
+
+        if '_atom_site_occupancy' in atomKeys:
+            occ = float(atom._atom_site_occupancy)
+            if not occ == 1.0:
+                if not fracOccFound: print(" ")
+                print("  WARNING: Fractional occupancy (" + str(occ) +") " \
+                      + "found for atom of type " + atomType + ".")
+                fracOccFound = True
+        else:
+            occ = 1.0
+        
+        # Some crystal structures obtained by neutron diffraction use D for H:
+        if atomType == 'D':
+            atomType = 'H'
+            atomLabel.replace('H','D')
+        
+        if ('_atom_site_symmetry_multiplicity' in atomKeys) and ('_atom_site_Wyckoff_symbol' in atomKeys):
+            atomTypes.append(atomType+' at '+atom._atom_site_symmetry_multiplicity+atom._atom_site_Wyckoff_symbol)
+        else:
+            atomTypes.append(atomType)
+        
+        atomPos = [atom._atom_site_fract_x, atom._atom_site_fract_y, atom._atom_site_fract_z]
+        for p in atomPos:
+            pp = p.split(".")
+            if len(pp) is 2:
+                decimals = p.split(".")[1]
+                if len(decimals) > 3 and len(decimals) < 6 and decimals[0] == decimals[1] and decimals[-1] != "0":
+                    print("\n---------------------\n"\
+                          +"  Warning: If the fractional coordinate "+p\
+                          +"  is a recurring decimal, such as 1/3,\n" \
+                          +"  then it is necessary to specify this value to six decimal places to be sure of \n" \
+                          +" it being recognised correctly as a spcecial position.\n  ------------------")
+		
+		# The coordinates of the atom (_atom_site_fract_x/y/z) may have 
+		# a last digit in parenthesis, like "0.6636(7)". Therefore we
+		# extract the part consisting of only digits and a decimal separator:
+                p = re.compile('[0-9.]*')
+        atomX = float(p.match(atom._atom_site_fract_x).group())
+        atomY = float(p.match(atom._atom_site_fract_y).group())
+        atomZ = float(p.match(atom._atom_site_fract_z).group())
+        
+        #atoms += "%s %f %f %f %f %f\n" % (atomType, atomX, atomY, atomZ, 0.0, occ)
+        atoms.append({'label': atomLabel, 'type': atomType, 'pos': (atomX,atomY,atomZ) })
+        firstAtom = False
+
+    if fracOccFound: 
+        print(" ")
+        print("ERROR: Fractional occupancies are not currently supported.\n")
+        exit()
+    
+        print("Atom types: " + ', '.join(atomTypes))
+    
+    return {'spacegroup': SG,
+            'unit_cell': [AA,BB,CC,alpha,beta,gamma],
+            'scatterers': atoms}
+    
 if __name__ == '__main__':
-    c = CIFReader()
-    c.read_cif('LaFeSi_small.cif')
-    print('Label: {0}'.format(c.get_label()))
-    print('---')
-    print('Cell: {0}'.format(c.get_cell()))
-    print('---')
-    print('Atoms:\n {0}'.format(c.get_atoms()))
+    mycif = 'LaFeSi_small.cif' #TODO: put an example CIF file in resources
+    if 1:
+        c = CIFReader()
+        c.read_cif(mycif)
+        print('Label: {0}'.format(c.get_label()))
+        print('---')
+        print('Cell: {0}'.format(c.get_cell()))
+        print('---')
+        print('Atoms:\n {0}'.format(c.get_atoms()))
+    if 0:
+        ### NOT WORKING ###
+        lfs = readCifFile(mycif)
+    if 1:
+        #from ASE
+        from ase import io
+        ats = io.read(mycif)
+    if 1:
+        #http://pymatgen.org/_modules/pymatgen/cli/feff_input_generation.html#main
+        from pymatgen.io.feff.sets import (FEFFDictSet, MPXANESSet, MPEXAFSSet)
+        from pymatgen.io.vasp import *
+        from pymatgen.io.cif import CifParser
+
+        r = CifParser(mycif)
+        calc_type = 'EXAFS' #'XANES'
+        if calc_type == 'EXAFS':
+            calc_set = MPEXAFSSet
+        else:
+            calc_set = MPXANESSet
+
+        abs_at = 'Fe'
+        struct = r.get_structures()[0]
+        _edge = 'K'
+        _radius = 8.
+        _comm = 'Feff from CIF file with pymatgen'
+
+        pmg = calc_set(abs_at, struct, edge=_edge, radius=_radius)
+        #pmg.write_input()
+        
+        #feffd = pmg.all_input()
+
+
+        
     pass
