8c07e68ee64ceb75a97cc4c8b6c42773f215ec6c
Mauro Rovezzi <mauro.rovezzi@gmail.com>
2018-11-04 09:09:58 +0100

    WIP: datagroup tests

---
+++ examples/datagroup_tests.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""DataGroup* tests
+===================
+
+"""
+import os, sys
+
+from sloth.collects.datagroup import DataGroup
+from sloth.collects.datagroup1D import DataGroup1D
+from sloth.collects.datagroup2D import DataGroup2D
+
+if __name__ == '__main__':
+    if 0:
+        d = DataGroup()
+    if 0:
+        d = DataGroup1D()
+    if 1:
+        d = DataGroup2D()
+    #EOF
+    pass
