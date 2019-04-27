ce91e910941002093f3c54281f4b635b44eceb16
Mauro Rovezzi <mauro.rovezzi@gmail.com>
2019-04-27 21:40:34 +0200

    refresh guis

--- sloth/gui/sloth_dataview.py
+++ sloth/gui/sloth_dataview.py
@@ -5,26 +5,30 @@
 =============================
 
 """
-import os, sys
+import os
+import sys
 from silx.gui import qt
 import sloth
 from sloth.gui.widgets.dataview import DataViewDockWidget
+
 
 class SlothDataViewMainWindow(qt.QMainWindow):
 
     def __init__(self, parent=None):
         super(SlothDataViewMainWindow, self).__init__(parent)
-        uiPath = os.path.join(sloth._resourcesPath, 'gui', 'uis', 'main_base.ui')
+        uiPath = os.path.join(sloth._resourcesPath, 'gui', 'uis',
+                              'main_base.ui')
         qt.loadUi(uiPath, baseinstance=self, package='sloth.gui')
         logoPath = os.path.join(sloth._resourcesPath, 'logo',
                                 'xraysloth_logo_03.svg')
         self.setWindowTitle('Sloth - dataview')
         self.setWindowIcon(qt.QIcon(logoPath))
 
-        #DATAVIEW DOCK WIDGET
+        # DATAVIEW DOCK WIDGET
         self.dataviewDockWidget = DataViewDockWidget(parent=self)
         self.addDockWidget(qt.Qt.LeftDockWidgetArea, self.dataviewDockWidget)
         self.dataviewDockWidget.setVisible(True)
+
 
 def sloth_dataview_app():
     app = qt.QApplication(sys.argv)
@@ -32,8 +36,9 @@ def sloth_dataview_app():
     appli = SlothDataViewMainWindow()
     appli.show()
     sys.exit(app.exec_())
-            
+
+
 if __name__ == '__main__':
-    #from silx import sx
-    #sx.enable_gui()
+    # from silx import sx
+    # sx.enable_gui()
     sloth_dataview_app()
--- sloth/gui/sloth_main.py
+++ sloth/gui/sloth_main.py
@@ -14,28 +14,23 @@ main = uic.loadUi('designer.ui')
 main.show()
 
 """
-from __future__ import absolute_import, division, unicode_literals
-
-import os, sys
-import math
-import numpy as np
-import matplotlib.pyplot as plt
+import os
+import sys
 
 from silx.gui import qt
 from silx.gui.widgets.PeriodicTable import PeriodicTable
 
-import sloth 
+import sloth
 from sloth.gui.widgets.console import (HAS_XRAYLIB, HAS_LARCH,
                                        _pushDict, _pushInfos,
                                        _slothKit)
 
 from sloth.gui.widgets.console import customIPythonWidget
-from sloth.gui.widgets.plot1D import customPlotWidget
+from sloth.gui.widgets.plot1D import SlothPlot1D
 
-#from .models.list_model import PaletteListModel
-
-    
+# from .models.list_model import PaletteListModel
 #_resourcesPath = os.path.join(os.path.split(sloth.__file__)[0], 'resources')
+
 
 class SlothMainWindow(qt.QMainWindow):
 
@@ -49,23 +44,23 @@ class SlothMainWindow(qt.QMainWindow):
         self.setWindowTitle('Sloth {0}'.format(sloth.__version__))
         self.setWindowIcon(qt.QIcon(logoPath))
 
-        #About dialog
+        # About dialog
         self.aboutDialog = AboutDialog(parent=self)
         self.actionAbout.triggered.connect(self.openAboutDialog)
 
-        #EXIT with C-q
-        #self.actionExit = qt.QAction(('E&xit'), self)
+        # EXIT with C-q
+        # self.actionExit = qt.QAction(('E&xit'), self)
         self.actionExit.setShortcut(qt.QKeySequence("Ctrl+Q"))
-        #self.addAction(self.actionExit)
+        # self.addAction(self.actionExit)
         self.actionExit.triggered.connect(self.confirmClose)
 
-        #OPEN with C-o
-        #self.actionOpen = qt.QAction(('Open'), self)
+        # OPEN with C-o
+        # self.actionOpen = qt.QAction(('Open'), self)
         self.actionOpen.setShortcut(qt.QKeySequence("Ctrl+O"))
-        #self.addAction(self.actionOpen)
+        # self.addAction(self.actionOpen)
         self.actionOpen.triggered.connect(self.selectFile)
-        
-        #CONSOLE WIDGET
+
+        # CONSOLE WIDGET
         self.consoleWidget = customIPythonWidget()
         self.consoleLayout.addWidget(self.consoleWidget)
         self.consoleWidget.pushVariables(_pushDict)
@@ -74,12 +69,12 @@ class SlothMainWindow(qt.QMainWindow):
         self.consoleWidget.pushVariables(_slothKit)
         for _key in _slothKit.keys():
             self.slothInfoListWidget.addItem(_key)
-            
-        #PLOT WIDGET
-        self.plotWidget = customPlotWidget()
+
+        # PLOT WIDGET
+        self.plotWidget = SlothPlot1D()
         self.plotLayout.addWidget(self.plotWidget)
-        self.consoleWidget.pushVariables({'p1d' : self.plotWidget})
-        self.tabInfoListWidget.addItem('p1d : SILX plot1d widget')
+        self.consoleWidget.pushVariables({'p1d': self.plotWidget})
+        self.tabInfoListWidget.addItem('p1d: SILX plot1d widget')
 
         # #PERIODIC TABLE
         # self.periodicTable = PeriodicTable(selectable=False)
@@ -88,25 +83,24 @@ class SlothMainWindow(qt.QMainWindow):
         # self.tabInfoListWidget.addItem('pt : SILX periodic table widget')
         # self.periodicTable.sigElementClicked.connect(self.click_table)
 
-
-        # #Load single dataset
+        # Load single dataset
         # self._fnames = []
         # self._fname = None
         # self.buttonSelectFname.clicked.connect(self.selectFile)
 
 
         # #Loaded data
-        
         # red   = qt.QColor(255,0,0)
         # green = qt.QColor(0,255,0)
         # blue  = qt.QColor(0,0,255)
 
         # rowCount = 4
         # columnCount = 6
-   
-        #model = PaletteListModel([red, green, blue])
-        #model.insertRows(0, 5)
-        #self.listViewData.setModel(model)
+
+        # model = PaletteListModel([red, green, blue])
+        # model.insertRows(0, 5)
+        # self.listViewData.setModel(model)
+
 
     def click_table(self, element):
         self.ptInfoListWidget.addItem(element.name)
@@ -120,7 +114,7 @@ class SlothMainWindow(qt.QMainWindow):
         self.lineSelectedFname.setText(self._fname)
         if fname:
             self._fnames.append(fname[0])
-            self.consoleWidget.pushVariables({'fnames' : self._fnames})
+            self.consoleWidget.pushVariables({'fnames': self._fnames})
 
     def confirmClose(self):
         msg = qt.QMessageBox(self)
@@ -133,20 +127,24 @@ class SlothMainWindow(qt.QMainWindow):
             self.close()
             qt.QApplication.closeAllWindows()
 
+
 class AboutDialog(qt.QDialog):
 
     def __init__(self, parent=None):
         super(AboutDialog, self).__init__()
-        uiPath = os.path.join(sloth._resourcesPath, 'gui', 'uis', 'sloth_about.ui')
+        uiPath = os.path.join(sloth._resourcesPath, 'gui', 'uis',
+                              'sloth_about.ui')
         qt.loadUi(uiPath, baseinstance=self, package='sloth.gui')
         self.nameLabel.setText('Sloth {0}'.format(sloth.__version__))
-            
+
+
 def sloth_main_gui_app():
     app = qt.QApplication(sys.argv)
     app.setStyle("plastique")
     appli = SlothMainWindow()
     appli.show()
     sys.exit(app.exec_())
-            
+
+
 if __name__ == '__main__':
     sloth_main_gui_app()
