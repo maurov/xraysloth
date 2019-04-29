cff2bfd5c079d61f5d9fb0ee5cde078c20fae15e
Mauro Rovezzi <mauro.rovezzi@gmail.com>
2019-04-29 18:14:45 +0200

    docstrings to numpy style

--- sloth/io/datasource_spech5.py
+++ sloth/io/datasource_spech5.py
@@ -11,13 +11,13 @@ Requirements
 """
 import os
 import logging
+import numpy as np
 import silx
 
-########################
-### DataSourceSpecH5 ###
-########################
+
 class DataSourceSpecH5(object):
-    """Data source utility wrapper for a Spec file read as HDF5 object via silx.io.open"""
+    """Data source utility wrapper for a Spec file read as HDF5 object
+    via silx.io.open"""
 
     def __init__(self, fname=None):
         """init with file name and default attributes"""
@@ -54,36 +54,47 @@ class DataSourceSpecH5(object):
     def set_group(self, group_url=None):
         """select group url
 
-        Args:
-            group_url (str): hdf5 url with respect to / where scans are stored [None -> /scans]
+        Parameters
+        ----------
+        group_url : str (optional)
+            hdf5 url with respect to / where scans are stored [None -> /scans]
 
-        Returns:
-            none: sets attribute self._group_url
+        Returns
+        -------
+        none: sets attribute self._group_url
         """
         self._group_url = group_url
-        if group_url is not None: self._logger.info(f"selected group {self._group_url}")
+        if self._group_url is not None:
+            self._logger.info(f"selected group {self._group_url}")
 
     def set_scan(self, scan_n, scan_idx=1, group_url=None):
         """select a given scan number
 
-        Args:
-            scan_n (int): scan number
-            scan_idx (int): scan repetition index [1]
-            group_url (str): hdf5 url with respect to / where scans are stored [None -> /scans]
+        Parameters
+        ----------
+        scan_n : int
+            scan number
+        scan_idx : int (optional)
+            scan repetition index [1]
+        group_url : str
+            hdf5 url with respect to / where scans are stored [None -> /scans]
 
-        Returns:
-            none: set attributes self._scan_n, self._scan_str, self._scan_url, self._sg
+        Returns
+        -------
+        none: set attributes self._scan_n, self._scan_str, self._scan_url, self._sg
         """
         self._scan_n = scan_n
         self._scan_str = f"{scan_n}.{scan_idx}"
-        if group_url is not None: self.set_group(group_url)
+        if group_url is not None:
+            self.set_group(group_url)
         if self._group_url is not None:
             self._scan_url = f"{self._group_url}/{self._scan_str}"
         else:
             self._scan_url = f"{self._scan_str}"
         self._sg = self._sf[self._scan_url]
         self._scan_title = self.get_title()
-        self._logger.info(f"selected scan {self._scan_url}: '{self._scan_title}'")
+        self._logger.info(f"selected scan {self._scan_url}:\
+                          '{self._scan_title}'")
 
     def _list_from_url(self, url_str):
         """utility method to get a list from a scan url
@@ -107,13 +118,17 @@ class DataSourceSpecH5(object):
     def get_title(self, scan_n=None):
         """get title str for a given scan
 
-        Args:
-            scan_n (int): optional scan number [None -> self._scan_n]
+        Parameters
+        ----------
+        scan_n : int (optional)
+            scan number [None -> self._scan_n]
 
-        Returns:
-            title (str): scan title self._sg[self._title_url][()]
+        Returns
+        -------
+        title (str): scan title self._sg[self._title_url][()]
         """
-        if scan_n is not None: self.set_scan(scan_n)
+        if scan_n is not None:
+            self.set_scan(scan_n)
         return self._sg[self._title_url][()]
 
     def get_scan_axis(self):
@@ -124,22 +139,30 @@ class DataSourceSpecH5(object):
             _axisout = _title_splitted[2]
         _mots, _cnts = self.get_motors(), self.get_counters()
         if not ((_axisout in _mots) and (_axisout in _cnts)):
-            self._logger.warning(f"'{_axisout}' not present in counters and motors")
+            self._logger.warning(f"'{_axisout}' not present\
+                                 in counters and motors")
         return _axisout
 
     def get_array(self, cnt, scan_n=None, group_url=None):
         """get array of a given counter
 
-        Args:
-            cnt (str or int): counter name or index in the list of counters
-            scan_n (int): optional scan number [None -> self._scan_n]
-            group_url (str): optional group name [None -> self._group_url]
+        Parameters
+        ----------
+        cnt : str or int
+            counter name or index in the list of counters
+        scan_n : int (optional)
+            optional scan number [None -> self._scan_n]
+        group_url : str (optional)
+            group name [None -> self._group_url]
 
-        Returns:
-            array
+        Returns
+        -------
+        array
         """
-        if group_url is not None: self.set_group(group_url)
-        if scan_n is not None: self.set_scan(scan_n)
+        if group_url is not None:
+            self.set_group(group_url)
+        if scan_n is not None:
+            self.set_scan(scan_n)
         cnts = self.get_counters()
         if type(cnt) is int:
             cnt = cnts[cnt]
@@ -155,16 +178,23 @@ class DataSourceSpecH5(object):
     def get_value(self, mot, scan_n=None, group_url=None):
         """get motor position
 
-        Args:
-            mot (str or int): motor name or index in the list of motors
-            scan_n (int): optional scan number [None -> self._scan_n]
-            group_url (str): optional group name [None -> self._group_url]
+        Parameters
+        ----------
+        mot : str or int
+            motor name or index in the list of motors
+        scan_n : int (optional)
+            optional scan number [None -> self._scan_n]
+        group_url : str (optional)
+            group name [None -> self._group_url]
 
-        Returns:
-            value
+        Returns
+        -------
+        value
         """
-        if group_url is not None: self.set_group(group_url)
-        if scan_n is not None: self.set_scan(scan_n)
+        if group_url is not None:
+            self.set_group(group_url)
+        if scan_n is not None:
+            self.set_scan(scan_n)
         mots = self.get_motors()
         if type(mot) is int:
             mot = mots[mot]
@@ -173,5 +203,6 @@ class DataSourceSpecH5(object):
             sel_mot = f'{self._mots_url}/{mot}'
             return self._sg[sel_mot][()]
         else:
-            self._logger.error(f"'{mot}' not found among the available motors:\n {mots}")
+            self._logger.error(f"'{mot}' not found in available motors:\n\
+                               {mots}")
             return 0
