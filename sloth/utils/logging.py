3e073e2704d8a60d0b39f50c352d74ee6c975e38
Mauro Rovezzi <mauro.rovezzi@gmail.com>
2019-05-08 07:40:23 +0200

    wip setting formatter

--- sloth/utils/logging.py
+++ sloth/utils/logging.py
@@ -7,21 +7,10 @@ import sys
 import logging
 # import tempfile
 
-# # set up basic logging configureation to temporary file
-# _default_format = '%(asctime)s [%(name)-s] %(levelname)-s : %(message)s'
-# # needs colorlog, not enabled yet!
-# _default_colors = {'DEBUG': 'cyan',
-#                    'INFO': 'green',
-#                    'WARNING': 'yellow',
-#                    'ERROR':    'red',
-#                    'CRITICAL': 'red'}
-#
-# logging.basicConfig(level=logging.DEBUG,
-#                     format=_default_format,
-#                     datefmt='%m-%d %H:%M',
-#                     filename='{0}.log'.format(tempfile.mktemp()),
-#                     filemode='w')
-
+# set up default logging configureation
+_default_format = '[%(name)-s] %(levelname)-s : %(message)s'
+_default_format_file = '%(asctime)s [%(name)-s] %(levelname)-s : %(message)s'
+_default_datefmt = '%Y-%m-%d %H:%M'
 _levels = {'DEBUG': logging.DEBUG,
            'INFO': logging.INFO,
            'WARNING': logging.WARNING,
@@ -29,9 +18,28 @@ _levels = {'DEBUG': logging.DEBUG,
            'FATAL': logging.FATAL,
            'CRITICAL': logging.CRITICAL}
 
+# # needs colorlog, not enabled yet!
+# _default_colors = {'DEBUG': 'cyan',
+#                    'INFO': 'green',
+#                    'WARNING': 'yellow',
+#                    'ERROR':    'red',
+#                    'CRITICAL': 'red'}
+#
+
+
+def enable_basicConfig(level='INFO'):
+    """logging basic configuration"""
+    logging.basicConfig(level=_levels[level],
+                        format=_default_format,
+                        datefmt=_default_datefmt)
+                        # filename='{0}.log'.format(tempfile.mktemp()),
+                        # filemode='w')
+
 
 def getLogger(name, level='INFO'):
     """Utility function to get the logger with customization
+
+    .. warning:: NOT WORKING AS EXPECTED -> FIXME!!!
 
     Parameters
     ----------
@@ -46,25 +54,29 @@ def getLogger(name, level='INFO'):
         - 'notebook'
             stream=sys.stdout
     """
+    enable_basicConfig(level=level)
     logger = logging.getLogger(name)
     logger.setLevel(_levels[level])
-
-    handler = logging.StreamHandler(sys.stderr)
-    """stderr handler (needed to show log in Jupyter notebook)"""
-
-    formatter = logging.Formatter('[%(name)-s] %(levelname)-s : %(message)s')
-    handler.setFormatter(formatter)
-    """Create formatter and add it to the handler"""
-
-    # Set STDERR handler as the only handler
-    logger.handlers = [handler]
-
     return logger
 
+    # handler = logging.StreamHandler(sys.stderr)
+    # """stderr handler (needed to show log in Jupyter notebook)"""
 
-def test_logger():
+    # formatter = logging.Formatter(_default_format)
+    # handler.setFormatter(formatter)
+    # """Create formatter and add it to the handler"""
+
+    # if (logger.hasHandlers()):
+    #     logger.handlers.clear()
+    # logger.addHandler(handler)
+    # """Clear existing handlers and add current one"""
+
+    # return logger
+
+
+def test_logger(level='DEBUG'):
     """Test custom logger"""
-    logger = getLogger('test', level=logging.DEBUG)
+    logger = getLogger('test', level=level)
     logger.debug('a debug message')
     logger.info('an info message')
     logger.warning('a warning message')
