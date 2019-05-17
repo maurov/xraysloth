de93a1956386d7f4bdf68e7f79e294579b0d823f
Mauro Rovezzi <mauro.rovezzi@gmail.com>
2019-05-17 07:49:39 +0200

    initial dataset works

--- sloth/groups/h5base.py
+++ sloth/groups/h5base.py
@@ -35,7 +35,7 @@ from sloth import __version__ as sloth_version
 
 #: module logger
 from sloth.utils.logging import getLogger
-_logger = getLogger('sloth.groups.h5base')
+_logger = getLogger('sloth.groups.h5base', level='DEBUG')
 
 ##########
 # GROUPS #
@@ -50,18 +50,26 @@ class BaseGroup(commonh5.Group):
         super(BaseGroup, self).__init__(name,
                                         parent=parent, attrs=attrs)
 
-    def add_entry(self, name):
-        """Add EntryGroup"""
-        self.add_node(EntryGroup(name, parent=self))
+    def add_group(self, name, attrs=None, cls=None):
+        """Add Group"""
+        if cls is None:
+            cls = EntryGroup
+        self.add_node(cls(name, attrs=attrs, parent=self))
 
-    def get_children(self):
+    def add_dataset(self, name, data, attrs=None, cls=None):
+        """Add Group"""
+        if cls is None:
+            cls = BaseDataset
+        self.add_node(cls(name, data, attrs=attrs, parent=self))
+
+    def _get_children(self):
         """List of children"""
         return list(self._get_items().values())
 
     def __str__(self, level=0):
         """Tree representation"""
-        ret = "+" + "---"*level + self.basename + "\n"
-        for child in self.get_children():
+        ret = f"{'|'*level}+---{self.basename}\n"
+        for child in self._get_children():
             ret += child.__str__(level+1)
         return ret
 
@@ -88,11 +96,13 @@ class EntryGroup(BaseGroup):
     """
 
     def __init__(self, name, parent=None, attrs=None):
-        """Constructor with default NXentry class"""
+        """Constructor simply adding default NXentry class"""
 
         _attrs = {"NX_class": "NXentry"}
         if attrs is not None:
             attrs.update(_attrs)
+        else:
+            attrs = _attrs
 
         super(EntryGroup, self).__init__(name, parent=parent, attrs=attrs)
 
@@ -106,20 +116,40 @@ class BaseDataset(commonh5.Dataset):
     """Base dataset used to insert data in a group"""
 
     def __init(self, name, data, parent=None, attrs=None):
-        """Constructor"""
+        """Constructor simply adding default NXdata class"""
+
+        _attrs = {"NX_class": "NXdata"}
+        if attrs is not None:
+            attrs.update(_attrs)
+        else:
+            attrs = _attrs
+
         super(BaseDataset, self).__init__(name, data,
                                           parent=parent, attrs=attrs)
+
+    def __str__(self, level=0):
+        """Dataset representation"""
+        ret = f"{'|'*level}----{self.basename}\n"
+        return ret
 
 
 def test_example():
     """Test example for :mod:`sloth.groups.h5base`"""
     _logger.info("Data model example: 't' is the root instance")
     t = RootGroup('test')
-    t.add_node(EntryGroup('entry1', parent=t))
-    t.add_entry('entry2')
-    t['entry1'].add_entry('subentry1')
-    t['entry2'].add_entry('subentry2')
-    t['entry2/subentry2'].add_entry('subsubentry2')
+    t.add_group('entry1', cls=EntryGroup)
+    t.add_group('entry2')
+    t['entry1'].add_group('subentry1')
+    t['entry2'].add_group('subentry2')
+    t['entry2/subentry2'].add_group('subsubentry2')
+
+    #: +dataset
+    import numpy as np
+    x = np.arange(10)
+    t['entry1'].add_dataset('x', x)
+    t['entry1/subentry1'].add_dataset('x', x)
+
+    _logger.info('print(t):\n%s', t)
     return t
 
 
