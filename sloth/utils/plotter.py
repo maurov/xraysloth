1c0b5b786ba5637932b7f5b858e7b2205a789628
Mauro Rovezzi <mauro.rovezzi@gmail.com>
2019-09-01 10:28:00 +0200

    initial mpl simple plotter for notebook

---
+++ sloth/utils/plotter.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Matplotlib plotter
+==================
+
+This utility is a pure matplotlib visualizer/plotter (no interaction!, no
+widgets). It is intended to be used in Jupyter notebooks (%inline or %notebook).
+
+"""
+from itertools import cycle
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import rcParams
+from sloth.utils.logging import getLogger
+
+
+class Plotter(object):
+
+    #: default colors palette
+    _colors = cycle(['#1F77B4', '#AEC7E8', '#FF7F0E', '#FFBB78',
+                     '#2CA02C', '#98DF8A', '#D62728', '#FF9896',
+                     '#9467BD', '#C5B0D5', '#8C564B', '#C49C94',
+                     '#E377C2', '#F7B6D2', '#7F7F7F', '#C7C7C7',
+                     '#BCBD22', '#DBDB8D', '#17BECF', '#9EDAE5'])
+
+    #: default plot labels
+    _labels = dict(x='X',
+                   y='Y')
+
+    def __init__(self, name='plotter',
+                 dpi=150, figsize=(10, 10),
+                 ncols=1, nrows=1,
+                 font_size=6,
+                 axes_linewidth=0.5,
+                 lines_linewidth=1.5,
+                 title=None, titles=None,
+                 logger=None):
+        """Logger Constructor
+
+        Parameters
+        ----------
+
+        name : str
+            name of the figure ['plotter']
+        dpi : int
+            figure dpi [150]
+        figsize : tuple
+            sigure size [(10, 10)]
+        font_size : int
+            base font size [6]
+        axes_linewidth : float
+            base axes width [0.5]
+        lines_linewidth : float
+            base lines width [1.5]
+        title : None or str
+            figure main title [None -> use self._name]
+        titles : None or list
+            list of titles for the subplots [None -> 'win=#']
+        """
+
+        #: general
+        self._name = name
+        self._logger = logger or getLogger(self._name)
+        self._title = title or self._name
+        self._titles = titles
+
+        #: matplotlib rcParams
+        self._text_usetex = False
+        self._font_size = font_size
+        self._axes_linewidth = axes_linewidth
+        self._lines_linewidth = lines_linewidth
+
+        #: figure/axes parameters
+        self._dpi = dpi
+        self._figsize = figsize
+        self._ncols = ncols
+        self._nrows = nrows
+        self._nplots = self._nrows*self._ncols
+
+        self._initRcParams()
+        self._initPlots()
+
+    def _initRcParams(self, style='seaborn-ticks'):
+        """init default Matplotlib parameters"""
+        plt.style.use(style)
+        rcParams['text.usetex'] = self._text_usetex
+        rcParams['font.size'] = self._font_size
+        rcParams['axes.titlesize'] = 'large'
+        rcParams['axes.linewidth'] = self._axes_linewidth
+        rcParams['xtick.major.width'] = self._axes_linewidth
+        rcParams['ytick.major.width'] = self._axes_linewidth/2.
+        rcParams['figure.dpi'] = self._dpi
+        rcParams['figure.figsize'] = self._figsize
+        rcParams['grid.alpha'] = 0.5
+        rcParams['lines.linewidth'] = self._lines_linewidth
+
+    def _initPlots(self):
+        """instantiate figure and subplots"""
+        plt.close(self._name)
+        self._fig, _axs = plt.subplots(num=self._name,
+                                       ncols=self._ncols,
+                                       nrows=self._nrows,
+                                       dpi=self._dpi,
+                                       figsize=self._figsize)
+        #: reshape Axes as list
+        self._axs = np.array(_axs).reshape(self._nplots)
+        self._fig.suptitle(f"Fig: {self._title}", fontsize=self._font_size+4)
+        self._initPlotsTitle(self._titles)
+
+    def _initPlotsTitle(self, titles=None):
+        """init title for all subplots"""
+        if titles is None:
+            self._titles = ['win={0}'.format(i) for i in range(self._nplots)]
+        else:
+            assert type(titles) is list, 'title should be a list'
+            assert len(titles) == self._nplots, 'title length should correspond to number of plot windows'
+            self._titles = titles
+        for iax, ax in enumerate(self._axs):
+            ax.set_title(self._titles[iax])
+
+    def _getNewColor(self):
+        return next(self._colors)
+
+    def getAxis(self, win):
+        """get the matplotlib.axes._subplots.AxesSubplot at given index"""
+        try:
+            return self._axs[win]
+        except IndexError:
+            self._logger.error('Wrong plot index')
+            return None
+
+    def subplots_adjust(self, **kws):
+        return self._fig.subplots_adjust(**kws)
+
+    def plot(self, x, y, label=None, win=0, color=None, side='left', **kws):
+        """plot in given axis"""
+        ax = self.getAxis(win)
+        if color is None:
+            color = self._getNewColor()
+        if side == 'right':
+            ax = ax.twinx()
+        ax.plot(x, y, label=label, color=color)
+        self._fig.tight_layout()
--- sloth/utils/xafsplotter.py
+++ sloth/utils/xafsplotter.py
@@ -9,94 +9,40 @@ widgets) for XAFS (x-ray absorption fine structure) data. It is intended to be
 used in Jupyter notebooks with Larch.
 
 """
-from itertools import cycle
-import numpy as np
-import matplotlib.pyplot as plt
-from sloth.utils.logging import getLogger
+from .plotter import Plotter
 
-class XAFSPlotter(object):
 
-    #: default colors palette
-    _colors = cycle(['#1F77B4', '#AEC7E8', '#FF7F0E', '#FFBB78',
-                     '#2CA02C', '#98DF8A', '#D62728', '#FF9896',
-                     '#9467BD', '#C5B0D5', '#8C564B', '#C49C94',
-                     '#E377C2', '#F7B6D2', '#7F7F7F', '#C7C7C7',
-                     '#BCBD22', '#DBDB8D', '#17BECF', '#9EDAE5'])
+class XAFSPlotter(Plotter):
 
-    #: default plot labels
-    _labels = dict(k=r'$k \rm\,(\AA^{-1})$',
-                   r=r'$R \rm\,(\AA)$',
-                   energy=r'$E\rm\,(eV)$',
-                   mu=r'$\mu(E)$',
-                   norm=r'normalized $\mu(E)$',
-                   flat=r'flattened $\mu(E)$',
-                   deconv=r'deconvolved $\mu(E)$',
-                   dmude=r'$d\mu(E)/dE$',
-                   dnormde=r'$d\mu_{\rm norm}(E)/dE$',
-                   chie=r'$\chi(E)$',
-                   chikw=r'$k^{{{0:g}}}\chi(k) \rm\,(\AA^{{-{0:g}}})$',
-                   chir=r'$\chi(R) \rm\,(\AA^{{-{0:g}}})$',
-                   chirmag=r'$|\chi(R)| \rm\,(\AA^{{-{0:g}}})$',
-                   chirre=r'${{\rm Re}}[\chi(R)] \rm\,(\AA^{{-{0:g}}})$',
-                   chirim=r'${{\rm Im}}[\chi(R)] \rm\,(\AA^{{-{0:g}}})$',
-                   chirpha=r'${{\rm Phase}}[\chi(R)] \rm\,(\AA^{{-{0:g}}})$',
-                   e0color='#B2B282',
-                   chirlab=None)
+    def __init__(self, name='XAFSplotter', data=None, **kws):
 
-    def __init__(self, data_group=None, name='XAFSplotter',
-                 dpi=150, figsize=(10, 10), ncols=1, nrows=1, title=None,
-                 logger=None):
+        super(XAFSPlotter, self).__init__(name=name, **kws)
+        self._data = data
 
-        self._name = name
-        self._logger = logger or getLogger(self._name)
+    def _updateLabels(self, labels=None):
+        """update default plot labels"""
+        #: default plot labels
+        if labels is None:
+            labels = dict(k=r'$k \rm\,(\AA^{-1})$',
+                          r=r'$R \rm\,(\AA)$',
+                          energy=r'$E\rm\,(eV)$',
+                          mu=r'$\mu(E)$',
+                          norm=r'normalized $\mu(E)$',
+                          flat=r'flattened $\mu(E)$',
+                          deconv=r'deconvolved $\mu(E)$',
+                          dmude=r'$d\mu(E)/dE$',
+                          dnormde=r'$d\mu_{\rm norm}(E)/dE$',
+                          chie=r'$\chi(E)$',
+                          chikw=r'$k^{{{0:g}}}\chi(k) \rm\,(\AA^{{-{0:g}}})$',
+                          chir=r'$\chi(R) \rm\,(\AA^{{-{0:g}}})$',
+                          chirmag=r'$|\chi(R)| \rm\,(\AA^{{-{0:g}}})$',
+                          chirre=r'${{\rm Re}}[\chi(R)] \rm\,(\AA^{{-{0:g}}})$',
+                          chirim=r'${{\rm Im}}[\chi(R)] \rm\,(\AA^{{-{0:g}}})$',
+                          chirpha=r'${{\rm Phase}}[\chi(R)] \rm\,(\AA^{{-{0:g}}})$',
+                          e0color='#B2B282',
+                          chirlab=None)
+        self._labels.update(labels)
 
-        #: figure/axes parameters
-        self._dpi = dpi
-        self._figsize = figsize
-        self._ncols = ncols
-        self._nrows = nrows
 
-        self._data = data_group
-
-        self._fig, _axs = plt.subplots(num=self._name,
-                                       ncols=self._ncols,
-                                       nrows=self._nrows,
-                                       dpi=self._dpi,
-                                       figsize=self._figsize)
-
-        #: reshape Axes as list
-        self._nplots = self._nrows*self._ncols
-        self._axs = np.array(_axs).reshape(self._nplots)
-        self._initPlotsTitle(title)
-
-    def _getNewColor(self):
-        return next(self._colors)
-
-    def _initPlotsTitle(self, title=None):
-        """init title for all subplots"""
-        if title is None:
-            self._ptitles = ['win={0}'.format(i) for i in range(self._nplots)]
-        else:
-            assert type(title) is list, 'title should be a list'
-            assert len(title) == self._nplots, 'title length should correspond to number of plot windows'
-            self._ptitles = title
-        for iax, ax in enumerate(self._axs):
-            ax.set_title(self._ptitles[iax])
-
-    def getAxis(self, win):
-        """get the matplotlib.axes._subplots.AxesSubplot at given index"""
-        try:
-            return self._axs[win]
-        except IndexError:
-            self._logger.error('Wrong plot index')
-            return None
-
-    def plot(self, x, y, label=None, win=0, color=None, side='left', **kws):
-        """plot in given axis"""
-        ax = self.getAxis(win)
-        if color is None:
-            color = self._getNewColor()
-        if side == 'right':
-            ax = ax.twinx()
-        ax.plot(x, y, label=label, color=color)
-        self._fig.tight_layout()
+if __name__ == '__main__':
+    pass
