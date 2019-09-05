72504a1bf8677e34af4318f0c0c15db79bba05f6
Mauro Rovezzi <mauro.rovezzi@gmail.com>
2019-09-05 17:59:20 +0200

    removed rebin (done outside of parser)

--- sloth/io/n42.py
+++ sloth/io/n42.py
@@ -12,8 +12,7 @@ https://www.nist.gov/programs-projects/ansiieee-n4242-standard
 import numpy as np
 
 
-def parse_n42(fname, header_length=36, footer_length=4, to_energy=True,
-              rebin_size=None):
+def parse_n42(fname, header_length=36, footer_length=4, to_energy=True):
     """Parse a N42 file
 
     Parameters
@@ -26,8 +25,6 @@ def parse_n42(fname, header_length=36, footer_length=4, to_energy=True,
         line number at which the footer starts [4]
     - to_energy : bool
         convert to energy (eV) [True]
-    - rebin_size : int or None
-        if given, it rebins to the given size [None]
     """
     def _getfloat(value):
         return float(value.replace(',', '.').split(' ')[0])
@@ -41,16 +38,6 @@ def parse_n42(fname, header_length=36, footer_length=4, to_energy=True,
     # footer = [line for line in lines[-footer_length:]]
     ydata = np.array([int(line) for line in lines[header_length:-footer_length]])
     xdata = np.array(range(len(ydata)))
-
-    #: rebin
-    if rebin_size is not None:
-        assert type(rebin_size) is int, "Rebin size should be an integer."
-        _x1 = np.array(range(len(ydata)+1))
-        _x2 = np.array(range(rebin_size))
-        from sloth.utils.arrays import rebin_piecewise_constant
-        _y2 = rebin_piecewise_constant(_x1, ydata, _x2)
-        xdata = np.array(range(len(_y2)))
-        ydata = _y2
 
     #: energy conversion coefficients
     ene_calib = lines[17].split('\t')[-1].split(' ')
