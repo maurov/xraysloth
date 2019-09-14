3a06b525519ba94b89f8917edfcd1ee44123e7af
Mauro Rovezzi <mauro.rovezzi@gmail.com>
2019-09-14 07:58:09 +0200

    show energy of the line

--- sloth/utils/xdata.py
+++ sloth/utils/xdata.py
@@ -362,7 +362,8 @@ def fluo_width(elem=None, line=None, herfd=False, showInfos=True):
         lw_xes = xl.AtomicLevelWidth(elm[1], getattr(xl, ln[3]+'_SHELL'))*1000
         lw_herfd = 1./(math.sqrt(lw_xas**2 + lw_xes**2))
         if showInfos:
-            _logger.info(f"{elm[0]} {line} (={ln[1]}):")
+            ln_ev = xl.LineEnergy(elm[1], getattr(xl, line+'_LINE'))*1000
+            _logger.info(f"{elm[0]} {line} (={ln[1]}): {ln_ev:.2f} eV")
             _logger.info(f"Atomic levels widths: XAS={lw_xas:.2f} eV, XES={lw_xes:.2f} eV, [HERFD={lw_herfd:.2f} eV]")
         if herfd is True:
             return lw_herfd
