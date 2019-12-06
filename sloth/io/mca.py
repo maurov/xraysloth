fade62268c5e069e35e42a144a0d432dedb6955f
Mauro Rovezzi <mauro.rovezzi@gmail.com>
2019-12-06 10:31:13 +0100

    minor clean

--- sloth/io/mca.py
+++ sloth/io/mca.py
@@ -15,7 +15,7 @@ def parse_mca(fname, header_length=38, to_energy=True):
 
     with open(fname) as f:
         lines = f.read().splitlines()
-    header = [line.split(' = ') for line in lines[:header_length]]
+    header = [line.split(" = ") for line in lines[:header_length]]
     ydata = np.array([int(line) for line in lines[header_length:]])
     xdata = np.array(range(len(ydata)))
 
@@ -27,29 +27,34 @@ def parse_mca(fname, header_length=38, to_energy=True):
         except Exception:
             pass
 
-    def _getfloat(value):
-        return float(value.replace(',', '.').split(' ')[0])
+    def _getfloat(valstr):
+        """Return float form string"""
+        #import locale
+
+        #locale.setlocale(locale.LC_ALL, "en_US.UTF-8")
+        #return locale.atof(valstr.split(" ")[0])
+        return float(valstr.replace(",", ".").split(" ")[0])
 
     #: write ICR/OCR and DT
-    icr = _getfloat(hdict['Input Count Rate'])*1000
-    ocr = _getfloat(hdict['Output Count Rate'])*1000
-    rt = _getfloat(hdict['Realtime'])
-    dt = (icr/ocr)*rt
-    hdict['ICR'] = icr
-    hdict['OCR'] = ocr
-    hdict['DT'] = dt
+    icr = _getfloat(hdict["Input Count Rate"]) * 1000
+    ocr = _getfloat(hdict["Output Count Rate"]) * 1000
+    rt = _getfloat(hdict["Realtime"])
+    dt = (icr / ocr) * rt
+    hdict["ICR"] = icr
+    hdict["OCR"] = ocr
+    hdict["DT"] = dt
 
     #: get FWHM
-    fwhm = _getfloat(hdict['ROI0 FWHM'])
-    hdict['FWHM'] = fwhm*1000
+    fwhm = _getfloat(hdict["ROI0 FWHM"])
+    hdict["FWHM"] = fwhm * 1000
 
     #: peaking time
-    pkt = _getfloat(hdict['Peaking Time'])
-    hdict['PT'] = pkt
+    pkt = _getfloat(hdict["Peaking Time"])
+    hdict["PT"] = pkt
 
     #: convert to energy
     if to_energy:
-        mca2ev = _getfloat(hdict['MCA Bin Width'])
+        mca2ev = _getfloat(hdict["MCA Bin Width"])
         xdata = xdata * mca2ev
 
     return xdata, ydata, hdict
