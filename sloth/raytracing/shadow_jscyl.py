c5e02d8bfe7e331b2b63524a221cf786598f5703
Mauro Rovezzi <mauro.rovezzi@gmail.com>
2020-01-06 07:19:34 +0100

    cleaning jscyl

--- sloth/raytracing/shadow_jscyl.py
+++ sloth/raytracing/shadow_jscyl.py
@@ -5,29 +5,48 @@
 
 """
 import math
-import logging
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+from sloth.utils.logging import getLogger
+from sloth.utils.bragg import bragg_ev
+from sloth.raytracing.shadow_utils import get_src_hdiv, get_src_vdiv, rotate_rays
+
+_LOGGER = getLogger("shadow_jscyl", level="DEBUG")
 
 HAS_SHADOW = False
 try:
     import Shadow
+
     Shadow.ShadowTools.plt.ion()
     HAS_SHADOW = True
-except:
-    logger.warning("Shadow not found!")
+except ImportError:
+    _LOGGER.warning("Shadow not found!")
     pass
 
-from sloth.raytracing.shadow_utils import (get_src_hdiv, get_src_vdiv, rotate_rays)
 
-#############
-### JSCYL ###
-#############
-def jscyl(d=None, refl_file=None, theta0=75., R=1000., nrays=50000,
-          aw=25., ah=80., ene0=None, ene0_hw=5., src_ovf=0.001,
-          src_shape='rectangle', src_x=0.2, src_z=0.1, src_rot=False,
-          move=None, run=True, **kwargs):
-    """raytrace Johansson cylindrical analyzer at a given theta angle
+#########
+# JSCYL #
+#########
+
+
+def jscyl(
+    d=None,
+    refl_file=None,
+    theta0=75.0,
+    R=1000.0,
+    nrays=50000,
+    aw=25.0,
+    ah=80.0,
+    ene0=None,
+    ene0_hw=5.0,
+    src_ovf=0.001,
+    src_shape="rectangle",
+    src_x=0.2,
+    src_z=0.1,
+    src_rot=False,
+    move=None,
+    run=True,
+    **kwargs
+):
+    """raytrace a Johansson cylindrical analyzer at a given theta angle
 
     Notes
     -----
@@ -52,7 +71,7 @@ def jscyl(d=None, refl_file=None, theta0=75., R=1000., nrays=50000,
         Bending radius (= diameter Rowland circle) in mm [1000.]
     ene0 : float
         Source central energy in eV [None]
-        if None, it is calculated from Bragg angle
+        if None, it is calculated from Bragg angle and d-spacing
     ene0_hw : float
         Source energy half bandwidth in eV [5]
     src_shape : str ['rectangle']
@@ -75,48 +94,51 @@ def jscyl(d=None, refl_file=None, theta0=75., R=1000., nrays=50000,
 
     Returns
     -------
-    beam, src, oe : :class:`Shadow.Beam`, :class:`Shadow.Source`, :class:`Shadow.OE`
+    (beam, src, oe) : tuple of :class:`Shadow.Beam`, :class:`Shadow.Source`, :class:`Shadow.OE`
 
     """
-    #CHECKS
-    if (HAS_SHADOW is False):
-        logger.error("Shadow not found!!!")
+    # CHECKS
+    if HAS_SHADOW is False:
+        _LOGGER.error("Shadow not found!!!")
         return (None, None, None)
-    if (d is None): raise NameError("d not given!")
-    if (refl_file is None): raise NameError("refl_file not given!")
+    if d is None:
+        raise NameError("d-spacing not given!")
+    if refl_file is None:
+        raise NameError("refl_file not given!")
     #
-    iwrite = 0 #write start/end/begin/star files (0=No, 1=Yes) [0]
-    p = R*math.sin(math.radians(theta0)) #SYMMETRIC RC GEOMETRY
-    #set src energy
-    if (ene0 is None): ene0 = bragg_ev(d, theta0)
+    iwrite = 0  # write start/end/begin/star files (0=No, 1=Yes) [0]
+    p = R * math.sin(math.radians(theta0))  # SYMMETRIC RC GEOMETRY
+    # set src energy
+    if ene0 is None:
+        ene0 = bragg_ev(d, theta0)
     ph1 = ene0 - ene0_hw
     ph2 = ene0 + ene0_hw
-    #set src divergence
-    hdiv = get_src_hdiv(aw/2, p) + 1.5*src_ovf
-    vdiv = get_src_vdiv(ah/2., p, theta0) + 3.5*src_ovf
+    # set src divergence
+    hdiv = get_src_hdiv(aw / 2, p) + 1.5 * src_ovf
+    vdiv = get_src_vdiv(ah / 2.0, p, theta0) + 3.5 * src_ovf
     # initialize shadow3 source (src) and beam
     beam = Shadow.Beam()
     src = Shadow.Source()
     oe = Shadow.OE()
-    # Define source variables. See meaning of variables in: 
+    # Define source variables. See meaning of variables in:
     #  https://raw.githubusercontent.com/srio/shadow3/master/docs/source.nml
-    src.ISTAR1 = 0 #seed 0=system clock
+    src.ISTAR1 = 0  # seed 0=system clock
     src.NPOINT = nrays
     #
-    src.FDISTR = 2 #uniform
+    src.FDISTR = 2  # uniform
     src.HDIV1 = hdiv
     src.HDIV2 = hdiv
     src.VDIV1 = vdiv
     src.VDIV2 = vdiv
     #
-    #src.FDISTR = 5 #conical
-    #src.CONE_MAX = 2*hdiv
+    # src.FDISTR = 5 #conical
+    # src.CONE_MAX = 2*hdiv
     #
-    if src_shape == 'point':
+    if src_shape == "point":
         src.FSOUR = 0
-    elif src_shape == 'rectangle':
+    elif src_shape == "rectangle":
         src.FSOUR = 1
-    elif src_shape == 'ellipse':
+    elif src_shape == "ellipse":
         src.FSOUR = 2
     else:
         raise NameError('src_shape "{0}" not understood!'.format(src_shape))
@@ -124,8 +146,8 @@ def jscyl(d=None, refl_file=None, theta0=75., R=1000., nrays=50000,
     src.WXSOU = src_x
     src.WZSOU = src_z
     #
-    src.F_COLOR = 3 #3=uniform energy distribution
-    src.F_PHOT = 0 #set eV units
+    src.F_COLOR = 3  # 3=uniform energy distribution
+    src.F_PHOT = 0  # set eV units
     src.PH1 = ph1
     src.PH2 = ph2
     #
@@ -134,26 +156,26 @@ def jscyl(d=None, refl_file=None, theta0=75., R=1000., nrays=50000,
     src.IDO_X_S = 0
     src.IDO_Y_S = 0
     src.IDO_Z_S = 0
-    # Define analyzer variables. See meaning of variables in: 
-    #  https://raw.githubusercontent.com/srio/shadow3/master/docs/oe.nml 
-    oe.DUMMY = 0.1 #mm
+    # Define analyzer variables. See meaning of variables in:
+    #  https://raw.githubusercontent.com/srio/shadow3/master/docs/oe.nml
+    oe.DUMMY = 0.1  # mm
     oe.FCYL = 1
     oe.FHIT_C = 1
-    oe.FILE_REFL = bytes(refl_file, 'utf8')
+    oe.FILE_REFL = bytes(refl_file, "utf8")
     oe.FMIRR = 1
     oe.FWRITE = 1
     oe.F_CRYSTAL = 1
     oe.F_EXT = 1
     oe.F_JOHANSSON = 1
-    oe.RLEN1 = ah/2.
-    oe.RLEN2 = ah/2.
-    oe.RMIRR = R/2.
-    oe.RWIDX1 = aw/2.
-    oe.RWIDX2 = aw/2.
+    oe.RLEN1 = ah / 2.0
+    oe.RLEN2 = ah / 2.0
+    oe.RMIRR = R / 2.0
+    oe.RWIDX1 = aw / 2.0
+    oe.RWIDX2 = aw / 2.0
     oe.R_JOHANSSON = R
     oe.T_IMAGE = p
-    oe.T_INCIDENCE = 90.-theta0
-    oe.T_REFLECTION = 90.-theta0
+    oe.T_INCIDENCE = 90.0 - theta0
+    oe.T_REFLECTION = 90.0 - theta0
     oe.T_SOURCE = p
     if move is not None:
         oe.F_MOVE = 1
@@ -163,47 +185,50 @@ def jscyl(d=None, refl_file=None, theta0=75., R=1000., nrays=50000,
         oe.X_ROT = move[3]
         oe.Y_ROT = move[4]
         oe.Z_ROT = move[5]
-        if (not src_rot):
-            #adjust src divergence -> EMPIRICAL ADJUSTMENTS!!!
-            hdiv1 = get_src_hdiv(1.05*move[0]+aw/2, p) + 7*src_ovf
-            hdiv2 = -1*(get_src_hdiv(1.05*move[0]-aw/2, p) - 5*src_ovf)
-            vdiv1 = get_src_vdiv(2.5*move[2]+ah/2., p, theta0) + 5*src_ovf
-            vdiv2 = get_src_vdiv(0.5*move[2]+ah/2., p, theta0) + src_ovf
+        if not src_rot:
+            # adjust src divergence -> EMPIRICAL ADJUSTMENTS!!!
+            hdiv1 = get_src_hdiv(1.05 * move[0] + aw / 2, p) + 7 * src_ovf
+            hdiv2 = -1 * (get_src_hdiv(1.05 * move[0] - aw / 2, p) - 5 * src_ovf)
+            vdiv1 = get_src_vdiv(2.5 * move[2] + ah / 2.0, p, theta0) + 5 * src_ovf
+            vdiv2 = get_src_vdiv(0.5 * move[2] + ah / 2.0, p, theta0) + src_ovf
             src.HDIV1 = hdiv1
             src.HDIV2 = hdiv2
             src.VDIV1 = vdiv1
             src.VDIV2 = vdiv2
-            #src.CONE_MAX = hdiv1
-            #rotate source -> NOT WORKING!!!
+            # src.CONE_MAX = hdiv1
+            # rotate source -> NOT WORKING!!!
         oe.FSTAT = 0
     if run:
-        #Run SHADOW to create the source
-        if iwrite: src.write("start.00")
+        # Run SHADOW to create the source
+        if iwrite:
+            src.write("start.00")
         beam.genSource(src)
         if (move is not None) and src_rot:
             rays = beam.rays.copy()
-            #rays_ry = rotate_rays(rays, move[4], 2)
-            _rz = math.atan(move[0]/p)
-            rz = math.degrees(_rz) * 0.5 #empirical factor to reduce!!!
+            # rays_ry = rotate_rays(rays, move[4], 2)
+            _rz = math.atan(move[0] / p)
+            rz = math.degrees(_rz) * 0.5  # empirical factor to reduce!!!
             rays_rz = rotate_rays(rays, rz, 3)
-            p1 = p/math.cos(_rz)
-            _rx = math.atan(move[5]/p1)
-            rx = -1*math.degrees(_rx) * 0.9 #empirical factor to reduce!!!
+            p1 = p / math.cos(_rz)
+            _rx = math.atan(move[5] / p1)
+            rx = -1 * math.degrees(_rx) * 0.9  # empirical factor to reduce!!!
             rays_rzrx = rotate_rays(rays_rz, rx, 1)
             beam.rays = rays_rzrx
         if iwrite:
             src.write("end.00")
             beam.write("begin.dat")
-        #Run optical element 1
-        #print("INFO: running optical element: %d"%(1))
-        if iwrite: oe.write("start.01")
-        beam.traceOE(oe,1)
+        # Run optical element 1
+        # _LOGGER.info("running optical element: %d"%(1))
+        if iwrite:
+            oe.write("start.01")
+        beam.traceOE(oe, 1)
         if iwrite:
             oe.write("end.01")
             beam.write("star.01")
-    logger.info('JsCBCA => R={0:.0f} mm, theta0={1:.3f}'.format(R, theta0))
-    logger.info('JsCBCA => p[q]={0:.4f} mm , ene0={1:.3f} eV'.format(p, ene0))
+    _LOGGER.info("JsCBCA => R={0:.0f} mm, theta0={1:.3f}".format(R, theta0))
+    _LOGGER.info("JsCBCA => p[q]={0:.4f} mm , ene0={1:.3f} eV".format(p, ene0))
     return (beam, src, oe)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     pass
