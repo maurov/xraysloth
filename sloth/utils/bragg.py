7b1816ac42d24ae064a7b6169746473f49398cc0
Mauro Rovezzi <mauro.rovezzi@gmail.com>
2020-01-22 07:09:54 +0100

    arg bug fix

--- sloth/utils/bragg.py
+++ sloth/utils/bragg.py
@@ -8,10 +8,11 @@ import warnings
 
 try:
     import scipy.constants.codata as const
+
     HAS_CODATA = True
     h = const.value("Planck constant in eV s")  # eV s
     c = const.value("speed of light in vacuum")  # m s^-1
-    HC = h*c
+    HC = h * c
 except ImportError:
     HAS_CODATA = False
     HC = 1.2398418743309972e-06  # eV * m
@@ -27,46 +28,46 @@ SIO2_C = 5.405
 
 def ev2wlen(energy):
     """convert photon energy (E, eV) to wavelength ($\lambda$, \AA$^{-1}$)"""
-    return (HC/energy)*1e10
+    return (HC / energy) * 1e10
 
 
 def wlen2ev(wlen):
     """convert photon wavelength ($\lambda$, \AA$^{-1}$) to energy (E, eV)"""
-    return (HC/wlen)*1e10
+    return (HC / wlen) * 1e10
 
 
 def kev2wlen(energy):
     """convert photon energy (E, keV) to wavelength ($\lambda$, \AA$^{-1}$)"""
-    return (HC/energy)*1e7
+    return (HC / energy) * 1e7
 
 
 def wlen2kev(wlen):
     """convert photon wavelength ($\lambda$, \AA$^{-1}$) to energy (E, keV)"""
-    return (HC/wlen)*1e7
+    return (HC / wlen) * 1e7
 
 
 def kev2ang(ene, d=0, deg=True):
     """energy (keV) to Bragg angle (deg/rad) for given d-spacing (\AA)"""
-    if d==0:
+    if d == 0:
         print("ERROR kev2deg: d-spacing is 0")
         return 0
     else:
-        _ang = np.arcsin((kev2wlen(ene))/(2*d))
-        if (deg is True):
+        _ang = np.arcsin((kev2wlen(ene)) / (2 * d))
+        if deg is True:
             _ang = np.rad2deg(_ang)
         return _ang
 
 
 def ang2kev(theta, d=0, deg=True):
     """Bragg angle (deg/rad) to energy (keV) for given d-spacing (\AA)"""
-    if (deg is True):
+    if deg is True:
         theta = np.deg2rad(theta)
-    return wlen2kev(2*d*np.sin(theta))
+    return wlen2kev(2 * d * np.sin(theta))
 
 
-def bragg_ev(d, theta, n=1):
+def bragg_ev(theta, d, n=1):
     """return the Bragg energy (eV) for a given d-spacing (\AA) and angle (deg)"""
-    return wlen2ev((2*d*np.sin(np.deg2rad(theta)))/n)
+    return wlen2ev((2 * d * np.sin(np.deg2rad(theta))) / n)
 
 
 def theta_b(wlen, d, n=1):
@@ -76,9 +77,9 @@ def theta_b(wlen, d, n=1):
         try:
             with warnings.catch_warnings():
                 warnings.simplefilter("ignore")
-                _thb = np.rad2deg(np.arcsin(((wlen*n)/(2*d))))
+                _thb = np.rad2deg(np.arcsin(((wlen * n) / (2 * d))))
             return _thb
-        except:
+        except Exception:
             return 0
     else:
         return 0
@@ -93,11 +94,12 @@ def bragg_th(ene, d, n=1):
 def xray_bragg(element, line, dspacing, retAll=False):
     """return the Bragg angle for a given element/line and crystal d-spacing"""
     from sloth.utils.xdata import xray_line
+
     line_ene = xray_line(element, line)
     try:
         theta = bragg_th(line_ene, dspacing)
-    except:
-        theta = '-'
+    except Exception:
+        theta = "-"
         pass
     if retAll:
         return (element, line, line_ene, theta)
@@ -120,37 +122,37 @@ def de_bragg(theta, dth):
 
 
 def sqrt1over(d2m):
-    if (d2m == 0):
+    if d2m == 0:
         return 0
     else:
-        return np.sqrt(1/d2m)
+        return np.sqrt(1 / d2m)
 
 
 def d_cubic(a, hkl, **kws):
     """d-spacing for a cubic lattice"""
     h, k, l = hkl[0], hkl[1], hkl[2]
-    d2m = (h**2 + k**2 + l**2) / a**2
+    d2m = (h ** 2 + k ** 2 + l ** 2) / a ** 2
     return sqrt1over(d2m)
 
 
 def d_tetragonal(a, c, hkl, **kws):
     """d-spacing for a tetragonal lattice"""
     h, k, l = hkl[0], hkl[1], hkl[2]
-    d2m = (h**2 + k**2) / a**2 + (l**2 / c**2)
+    d2m = (h ** 2 + k ** 2) / a ** 2 + (l ** 2 / c ** 2)
     return sqrt1over(d2m)
 
 
 def d_orthorhombic(a, b, c, hkl, **kws):
     """d-spacing for an orthorhombic lattice"""
     h, k, l = hkl[0], hkl[1], hkl[2]
-    d2m = (h**2 / a**2) + (k**2 / b**2) + (l**2 / c**2)
+    d2m = (h ** 2 / a ** 2) + (k ** 2 / b ** 2) + (l ** 2 / c ** 2)
     return sqrt1over(d2m)
 
 
 def d_hexagonal(a, c, hkl, **kws):
     """d-spacing for an hexagonal lattice"""
     h, k, l = hkl[0], hkl[1], hkl[2]
-    d2m = 4./3.*((h**2 + h*k + k**2) / a**2) + (l**2 / c**2)
+    d2m = 4.0 / 3.0 * ((h ** 2 + h * k + k ** 2) / a ** 2) + (l ** 2 / c ** 2)
     return sqrt1over(d2m)
 
 
@@ -158,11 +160,16 @@ def d_monoclinic(a, b, c, beta, hkl, **kws):
     """d-spacing for a monoclinic lattice"""
     h, k, l = hkl[0], hkl[1], hkl[2]
     rbeta = np.deg2rad(beta)
-    d2m = 1./np.sin(rbeta)**2\
-        * ((h**2 / a**2)\
-        + ((h**2 * np.sin(rbeta)**2) / b**2)\
-        + (l**2 / c**2)\
-        - ((2*h*l*np.cos(rbeta) / (a*c))))
+    d2m = (
+        1.0
+        / np.sin(rbeta) ** 2
+        * (
+            (h ** 2 / a ** 2)
+            + ((h ** 2 * np.sin(rbeta) ** 2) / b ** 2)
+            + (l ** 2 / c ** 2)
+            - ((2 * h * l * np.cos(rbeta) / (a * c)))
+        )
+    )
     return sqrt1over(d2m)
 
 
@@ -175,23 +182,34 @@ def d_triclinic(a, b, c, alpha, beta, gamma, hkl, **kws):
     cosralpha = np.cos(ralpha)
     cosrbeta = np.cos(rbeta)
     cosrgamma = np.cos(rgamma)
-    V = a * b * c * np.sqrt( 1 - cosralpha**2 - cosrbeta**2 - cosrgamma**2 \
-                             + 2 * cosralpha * cosrbeta * cosrgamma )
-    d2m = ( 1. / V**2 ) * \
-          ( h**2 * b**2 * c**2 * np.sin(ralpha)**2 \
-            + k**2 * a**2 * c**2 * np.sin(rbeta)**2 \
-            + l**2 * a**2 * b**2 * np.sin(rgamma)**2 \
-            + 2 * h * k * a * b * c**2 * ( cosralpha * cosrbeta - cosrgamma ) \
-            + 2 * k * l * a**2 * b * c * ( cosrbeta * cosrgamma  - cosralpha ) \
-            + 2 * h * l * a * b**2 * c * ( cosralpha * cosrgamma - cosrbeta ) )
+    V = (
+        a
+        * b
+        * c
+        * np.sqrt(
+            1
+            - cosralpha ** 2
+            - cosrbeta ** 2
+            - cosrgamma ** 2
+            + 2 * cosralpha * cosrbeta * cosrgamma
+        )
+    )
+    d2m = (1.0 / V ** 2) * (
+        h ** 2 * b ** 2 * c ** 2 * np.sin(ralpha) ** 2
+        + k ** 2 * a ** 2 * c ** 2 * np.sin(rbeta) ** 2
+        + l ** 2 * a ** 2 * b ** 2 * np.sin(rgamma) ** 2
+        + 2 * h * k * a * b * c ** 2 * (cosralpha * cosrbeta - cosrgamma)
+        + 2 * k * l * a ** 2 * b * c * (cosrbeta * cosrgamma - cosralpha)
+        + 2 * h * l * a * b ** 2 * c * (cosralpha * cosrgamma - cosrbeta)
+    )
     return sqrt1over(d2m)
 
 
 def get_dspacing(mat, hkl):
     """get d-spacing for Si or Ge and given reflection (hkl)"""
-    if mat == 'Si':
+    if mat == "Si":
         dspacing = d_cubic(SI_ALAT, hkl)
-    elif mat == 'Ge':
+    elif mat == "Ge":
         dspacing = d_cubic(GE_ALAT, hkl)
     else:
         print("ERROR get_dspacing: available materials -> 'Si' 'Ge'")
@@ -199,7 +217,7 @@ def get_dspacing(mat, hkl):
     return dspacing
 
 
-def findhkl(energy=None, thetamin=65., crystal='all', retAll=False):
+def findhkl(energy=None, thetamin=65.0, crystal="all", retAll=False):
     """findhkl: for a given energy (eV) finds the Si and Ge reflections
     with relative Bragg angle
 
@@ -229,32 +247,41 @@ def findhkl(energy=None, thetamin=65., crystal='all', retAll=False):
             are all odd or all even zincblende: as fcc but if all even
             (0 is even) then h+k+l = 4n
             """
-            #hkl = itertools.product(idx, idx, idx)
+            # hkl = itertools.product(idx, idx, idx)
             hkl = itertools.combinations_with_replacement(idx, 3)
             for x in hkl:
                 # check all even
-                if (x[0]%2 == 0 and x[1]%2 == 0 and x[2]%2 == 0) and not ((x[0]+x[1]+x[2])%4 == 0):
+                if (x[0] % 2 == 0 and x[1] % 2 == 0 and x[2] % 2 == 0) and not (
+                    (x[0] + x[1] + x[2]) % 4 == 0
+                ):
                     pass
                 else:
                     try:
                         theta = theta_b(ev2wlen(energy), d_cubic(alat, x))
-                    except:
+                    except Exception:
                         continue
-                    if (theta >= thetamin):
-                        print('{0}({1} {2} {3}), {4} {5:2.2f}'.format(crystal, x[0], x[1], x[2], 'Bragg', theta))
-                        if retAll: retDat.append((crystal, x[0], x[1], x[2], theta))
+                    if theta >= thetamin:
+                        print(
+                            "{0}({1} {2} {3}), {4} {5:2.2f}".format(
+                                crystal, x[0], x[1], x[2], "Bragg", theta
+                            )
+                        )
+                        if retAll:
+                            retDat.append((crystal, x[0], x[1], x[2], theta))
+
         # all permutations of odd (h,k,l)
         _structure_factor(reversed(range(1, HKL_MAX, 2)))
         # all permutations of even (h,k,l)
         _structure_factor(reversed(range(0, HKL_MAX, 2)))
-    if crystal == 'Si':
-        _find_theta('Si', SI_ALAT)
-    elif crystal == 'Ge':
-        _find_theta('Ge', GE_ALAT)
+
+    if crystal == "Si":
+        _find_theta("Si", SI_ALAT)
+    elif crystal == "Ge":
+        _find_theta("Ge", GE_ALAT)
     else:
-        _find_theta('Si', SI_ALAT)
-        _find_theta('Ge', GE_ALAT)
+        _find_theta("Si", SI_ALAT)
+        _find_theta("Ge", GE_ALAT)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     pass
