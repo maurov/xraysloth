d3d2a151a2632b0073acc02ab0dcd937d8448588
blissadm at rubis on bm16 <fame@esrf.fr>
2021-10-29 11:04:22 +0200

    get_rixs_bm16 moved to Larch

--- sloth/io/rixs_esrf_fame.py
+++ sloth/io/rixs_esrf_fame.py
@@ -14,136 +14,14 @@ import os
 import time
 import glob
 import numpy as np
-from sloth.io.specfile_reader import _mot2array
-from sloth.fit.peakfit_silx import fit_splitpvoigt
-from sloth.io.datasource_spech5 import DataSourceSpecH5
 from silx.io.dictdump import dicttoh5
 
+from larch.io.specfile_reader import DataSourceSpecH5, _mot2array
+from larch.io.rixs_esrf_fame import get_rixs_bm16
+
+from sloth.fit.peakfit_silx import fit_splitpvoigt
 from sloth.utils.logging import getLogger
 _logger = getLogger('io_rixs_bm16')
-
-def _parse_header(fname):
-    """Get parsed header for the RIXS_###.log file
-    Return
-    ------
-    header : dict
-    """
-    with open(fname) as f:
-        lines = f.read().splitlines()
-    header_lines = [line[1:] for line in lines if line[0] == '#']
-    header = {}
-    for line in header_lines:
-        ls = line.split(": ")
-        try:
-            k, v = ls[0], ls[1]
-        except IndexError:
-            pass
-        for s in ('START', 'END', 'STEP'):
-            if s in k:
-                v = float(v)
-        header[k] = v
-    return header
-
-def get_rixs_bm16(rixs_logfn, sample_name='UNKNOWN_SAMPLE', out_dir=None, counter_signal='absF1', counter_norm=None, interp_ene_in=True, save_rixs=False):
-    """Build RIXS map as X,Y,Z 1D arrays
-    Parameters
-    ----------
-    rixs_logfn : str
-        path to the RIXS_###.log file
-    sample_name : str, optional ['UNKNOWN_SAMPLE']
-    out_dir : str, optional
-        path to save the data [None -> data_dir]
-    counter_signal : str
-        name of the data column to use as signal
-    counter_norm : str
-        name of the data column to use as normaliztion
-    interp_ene_in: bool
-        perform interpolation ene_in to the energy step of ene_out [True]
-    save_rixs : bool
-        if True -> save outdict to disk (in 'out_dir')
-    Returns
-    -------
-    outdict : dict
-        {
-        '_x': array, energy in
-        '_y': array, energy out
-        '_z': array, signal
-        'writer_name': str,
-        'writer_version': str,
-        'writer_timestamp': str,
-        'filename_all' : list,
-        'filename_root': str,
-        'name_sample': str,
-        'name_scan': str,
-        'counter_all': str,
-        'counter_signal': str,
-        'counter_norm': str,
-        'ene_grid': float,
-        'ene_unit': str,
-        }
-    """
-    _writer = 'get_rixs_bm16'
-    _writer_version = '2021-04'  #: used for reading back in RixsData.load_from_h5()
-    _writer_timestamp = '{0:04d}-{1:02d}-{2:02d}_{3:02d}{4:02d}'.format(*time.localtime())
-    header = _parse_header(rixs_logfn)
-    sfn = header["DATAFILE"]
-    scntype = header["RIXS_SCAN_TYPE"]
-    data_dir = os.path.join(os.sep, *sfn.split('/')[1:-1])
-    if out_dir is None:
-        out_dir = data_dir
-    ds = DataSourceSpecH5(sfn, logger=_logger)
-
-    logobj = np.genfromtxt(logobj, delimiter=',', comments='#')
-    scans = logobj[:, 0]  # list of scan numers
-    enes = logobj[:, 1] * 1000  # in eV
-
-    _counter = 0
-    for scan, estep in zip(scans, enes):
-        try:
-            ds.set_scan(scan)
-            escan, sig, lab, attrs = ds.get_curve(counter_signal)
-        except Exception:
-            _logger.error(f"cannot load scan {scan}!")
-            continue
-        if scntype == 'rixs_et':
-            x = _mot2array(enestep, escan)
-            y = escan
-        else:
-            x = escan
-            y = _mot2array(enestep, escan)
-        if _counter == 0:
-            xcol = x
-            ycol = y
-            zcol = z
-        else:
-            xcol = np.append(xcol, x)
-            ycol = np.append(ycol, y)
-            zcol = np.append(zcol, z)
-        _counter += 1
-        _logger.info(f"Loaded scan {scan}: {estep} eV")
-
-    outdict = {
-        '_x': _xcol,
-        '_y': _ycol,
-        '_z': _zcol,
-        'writer_name': _writer,
-        'writer_version': _writer_version,
-        'writer_timestamp': _writer_timestamp,
-        'counter_signal': counter_signal,
-        'counter_norm': counter_norm,
-        'sample_name': sample_name,
-        'ene_unit': 'eV',
-        'rixs_header': header,
-        'data_dir': data_dir,
-        'out_dir': out_dir,
-        }
-
-    if save_rixs:
-        fnout = "{0}_rixs.h5".format(fnstr)
-        dicttoh5(outdict, os.path.join(out_dir, fnout))
-        _logger.info("RIXS saved to {0}".format(fnout))
-
-    return outdict
 
 def get_xyz_bm16(logobj, specobj, fit_elastic=False):
     """function to get 3 arrays representing the RIXS plane
@@ -168,6 +46,9 @@ def get_xyz_bm16(logobj, specobj, fit_elastic=False):
     xcol, ycol, zcol: 1D arrays
 
     """
+    
+    _logger.warning("!!!DEPRECATED!!! -> use `get_rixs_bm16` from larch.io.rixs_esrf_fame")
+
     if isinstance(logobj, str):
         logobj = np.genfromtxt(logobj, delimiter=',', comments='#')
     scans = logobj[:, 0]  # list of scan numers
