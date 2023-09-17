93c14144356cc44b75d7e12e5c28ffa740d3b23a
opd16@lbm16jade <mauro.rovezzi@esrf.fr>
2023-09-17 21:21:00 +0200

    wip: select std

--- sloth/utils/pymca.py
+++ sloth/utils/pymca.py
@@ -76,7 +76,7 @@ def get_average(iskip=1, method="average"):
 
 def get_std(estart):
     """get curves (remove) and calculate the std """
-    curves = get_curves(remove=True)
+    curves = get_curves()
     outcurves = []
     for icurve, (x, y, leg, info) in enumerate(curves):
         if estart is None:
@@ -87,7 +87,7 @@ def get_std(estart):
         print(f"{icurve}) {std:.4E}: {leg}")
         info["std"] = std
         outcurves.append((x, y, leg, info))  
-        #plugin.removeCurve(leg)
+        plugin.removeCurve(leg)
     return outcurves
 
 
@@ -127,32 +127,58 @@ def get_std(estart):
 
 def select_curves_by_std(std_frac=None, estart=None, plot=True, m=6):
     """push back curves from below std level"""
-    print("----- get all curves from plot:")
+    print("---> get all curves from plot:")
     curves = get_std(estart)
-    stds = [info['std'] for (x, y, leg, info) in curves]
-    stds = np.array(stds)
-    stds2 = reject_outliers(stds, m=m, return_ma=True)  #: detect outliers automagically
-    nstds = stds/np.std(stds) #normalized ???
+    stds0 = [info['std'] for (x, y, leg, info) in curves]
+    stds0 = np.array(stds0)
+    mstds0 = np.median(stds0)
+    if std_frac is not None:
+        max_std = mstds0 * (1 + std_frac)
+    else:
+        max_std = mstds0
+    beststd = stds0.min()
+    ibeststd = index_of(stds0, beststd)
+    (xbest, ybest, legbest, infobest) = curves[ibeststd]
+    print(f"---> *best std* {ibeststd}) {beststd:.4E}: {legbest}")
+    yrels = [(y - ybest) for (x, y, leg, info) in curves]
+    #stds1 = [np.std(yrel) for yrel in yrels]
+    #stds1 = [np.sum(np.abs(yrel)) for yrel in yrels]
+    #stds1 = [len(np.extract(yrel > beststd, yrel)) for yrel in yrels]
+    stds1 = stds0  #: all previous methods not working! TODO: find a clever way to discriminate curves
+    #DEV/DEBUG
+    if 0:
+        for yrel, (x, y, leg, info) in zip(yrels, curves):
+            plugin.addCurve(x, yrel, leg, info)
+        import pdb; pdb.set_trace()
+        return 
+    stds2 = reject_outliers(stds1, m=m, return_ma=True)  #: detect outliers automagically
+    for istd, std in enumerate(stds0):
+        if std <= mstds0:
+            stds2.mask[istd] = False  #: force to keep all curves with std less than median
+        if (std_frac is not None) and (std > max_std):
+            stds2.mask[istd] = True  #: force to remove if threshold is given
+    nstds = stds0/np.std(stds0) #normalized ???
     if plot:
         plt.ion()
-        plt.close("all")
+        #plt.close("all")
+        plt.clf()
         fig, ax = plt.subplots(num="stds")
         ax.set_title("standard deviations of curves")
-        #ax.plot(nstds, ls="--", marker="o", color="blue", fillstyle='none')
-        ax.plot(stds, ls="--", marker="o", color="blue", fillstyle='none', label="all")
-        ax.plot(stds2, ls=" ", marker="o", color="green", label=f"detected")
-        ax.hlines(std_frac, 0, len(curves), colors=['red'], ls='-')
-        ax.legend()
+        ax.plot(stds1, ls="--", marker="o", color="blue", fillstyle='none', label="all")
+        ax.plot(stds2, ls=" ", marker="o", color="green", label=f"selected")
+        ax.hlines(mstds0, 0, len(curves), colors=['orange'], ls='--', label=f"median: {mstds0:.3E}", linewidth=1)
         if std_frac is not None:
-            ax.set_ylim(nstds.min(), 2*nstds.min())
+            ax.set_ylim(stds0.min()*0.95, max_std*1.05)
+            ax.hlines(max_std, 0, len(curves), colors=['red'], ls='--', label=f"threshold: {max_std:.2E}", linewidth=1)
+        ax.legend(fontsize=8)
         ax.set_xlabel("index of curves")
         ax.set_ylabel("stds")
         ax.minorticks_on()
-        ax.xaxis.set_tick_params(which='minor', bottom=False)
-        ax.grid(True, axis="y", which="both", linewidth=0.5)
+        #ax.xaxis.set_tick_params(which='minor', bottom=False)
+        ax.grid(True, axis="both", which="both", linewidth=0.5)
     #if std_frac is not None:
     #std_level = (np.std(stds) * std_frac)
-    print(f"----- selected curves:")
+    print(f"---> selected curves:")
     for icurve, (x, y, leg, info) in enumerate(curves):
         std = info["std"]
         #print(f"{icurve}, {len(curve)}, {stds2.mask[icurve]}, {std}")
