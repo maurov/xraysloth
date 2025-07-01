a23c7fe52ad464310262883776e8b50e39ec1f5e
Mauro Rovezzi <mauro.rovezzi@gmail.com>
2025-07-01 17:52:59 +0200

    WIP refactor setup

--- sloth/__init__.py
+++ sloth/__init__.py
@@ -20,7 +20,7 @@ warnings.filterwarnings("ignore", message="numpy.dtype size changed")
 warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
 
 __author__ = "Mauro Rovezzi"
-__version__ = "2022.12"
+__version__ = "25.1.0"
 
 _libDir = os.path.dirname(os.path.realpath(__file__))
 _resourcesPath = os.path.join(_libDir, "resources")
